a75702b976934fc4f679605cfe0fec9b8088ed16
Ryan Joy <atxryan@gmail.com>
2013-01-28 11:31:13 -0800

    First commit for project

---
+++ App/MetroCarFinder/js/car2go.js
@@ -0,0 +1,9 @@
+﻿(function () {
+    "use strict";
+
+    var all_cars_url = "http://www.car2go.com/api/v2.1/vehicles?loc=austin&oauth_consumer_key=MetroCarFinder&format=json";
+
+
+    
+
+})();
---
+++ App/MetroCarFinder/js/settings.js
@@ -0,0 +1,91 @@
+﻿//// THIS CODE AND INFORMATION IS PROVIDED "AS IS" WITHOUT WARRANTY OF 
+//// ANY KIND, EITHER EXPRESSED OR IMPLIED, INCLUDING BUT NOT LIMITED TO 
+//// THE IMPLIED WARRANTIES OF MERCHANTABILITY AND/OR FITNESS FOR A 
+//// PARTICULAR PURPOSE. 
+//// 
+//// Copyright (c) Microsoft Corporation. All rights reserved 
+
+(function () {
+    "use strict";
+
+
+
+    
+    
+})();
+
+(function () {
+
+    var appLanguages = Windows.Globalization.ApplicationLanguages;
+    var languages = appLanguages.languages;
+    var manifestLanguages = appLanguages.manifestLanguages;
+    var override = appLanguages.primaryLanguageOverride;
+
+    // A custom WinJS control for letting users override language
+    // Default Option is the text displayed by default in the drop down (defaults to English text)
+    var DisplayLanguageOverridePicker = WinJS.Class.define(function (element, options) {
+        this.element = element;
+        element.winControl = this;
+
+        // Set defaults and then options
+        this.defaultOption = 'Use language preferences (recommended)';
+        WinJS.UI.setOptions(this, options);
+
+        // First show the default setting
+        this._addOption(this.defaultOption, "");
+
+        var that = this;
+
+        // If there are other languages the user speaks that aren't the default show them first before a separator
+        if (override !== '' || languages.size > 1) {
+            languages.forEach(function (langTag, index) {
+                if ((override === '' && index !== 0) || (override !== '' && index !== 1)) {
+                    that.addLanguage(langTag);
+                }
+            });
+            this._addOption("——————", "", true);
+        }
+
+        // Finally, add the rest of the languages the app supports
+        var list = new WinJS.Binding.List(manifestLanguages);
+        list.sort(function (a, b) {
+            var langA = new Windows.Globalization.Language(a).displayName;
+            var langB = new Windows.Globalization.Language(b).displayName;
+            return langA - langB;
+        });
+        list.forEach(function (langTag) {
+            that.addLanguage(langTag);
+        });
+
+        this.element.addEventListener("change", this._change, false);
+
+    }, {
+
+        addLanguage: function (langTag) {
+            var lang = new Windows.Globalization.Language(langTag);
+            var text = (lang.nativeName === lang.displayName) ? lang.displayName : lang.displayName + " - " + lang.nativeName;
+            this._addOption(text, langTag, false, (langTag && langTag === override));
+        },
+
+        _addOption: function (text, value, disabled, selected) {
+            var option = document.createElement('option');
+            option.value = value;
+            option.selected = selected || false;
+            option.disabled = disabled || false;
+            option.textContent = text;
+            this.element.appendChild(option);
+        },
+
+        // Sets the primary langauge override so resources, the host and other things attempt to load
+        // according to it first.
+        _change: function () {
+            appLanguages.primaryLanguageOverride = this.value;
+        }
+
+    });
+
+
+    WinJS.Namespace.define("Sample", {
+        DisplayLanguageOverridePicker: DisplayLanguageOverridePicker
+    });
+})();
