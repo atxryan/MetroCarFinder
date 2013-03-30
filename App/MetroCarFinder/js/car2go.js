c85a979a40e76039f557db9d03971b21f91c13a7
Ryan Joy <atxryan@gmail.com>
2013-03-30 13:12:56 -0600

    Cleaning up files.

--- App/MetroCarFinder/js/car2go.js
+++
@@ -1,9 +0,0 @@
-﻿(function () {
-    "use strict";
-
-    var all_cars_url = "http://www.car2go.com/api/v2.1/vehicles?loc=austin&oauth_consumer_key=MetroCarFinder&format=json";
-
-
-    
-
-})();
---
+++ App/MetroCarFinder/js/settings-about.js
@@ -0,0 +1,21 @@
+﻿(function () {
+    var page = WinJS.UI.Pages.define("/settings/about.html", {
+
+        ready: function (element, options) {
+            WinJS.Resources.processAll();
+
+            // Register the handlers for dismissal 
+            //document.getElementById("programmaticInvocationSettingsFlyout").addEventListener("keydown", handleAltLeft);
+           // document.getElementById("programmaticInvocationSettingsFlyout").addEventListener("keypress", handleBackspace);
+
+
+        },
+
+        unload: function () {
+            // Remove the handlers for dismissal 
+            //document.getElementById("programmaticInvocationSettingsFlyout").removeEventListener("keydown", handleAltLeft);
+            //document.getElementById("programmaticInvocationSettingsFlyout").removeEventListener("keypress", handleBackspace);
+        },
+    });
+
+})();
